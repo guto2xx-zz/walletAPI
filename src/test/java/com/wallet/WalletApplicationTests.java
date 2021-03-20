873a678f5428580d689efc90421329f164028a61
Guto Macedo <guto2xx@gmail.com>
2021-03-20 14:29:41 -0300

    Criado user service

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,21 +1,17 @@
 package com.wallet;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
 class WalletApplicationTests {
-
+	
+	@Test
 	void contextLoads() {
-		assertEquals(1, 1);
 	}
 
 }
